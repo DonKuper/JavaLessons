2eba955c93556041a98ede18843a291fe3ebd939
Ilya Kupriyanov <kup-92@yandex.ru>
2020-01-04 17:10:05 +0300

    basic (fetch и optional - надо разобраться)

--- jpa/src/main/java/ru/kuper/entities/Student.java
+++ jpa/src/main/java/ru/kuper/entities/Student.java
@@ -3,20 +3,27 @@ package ru.kuper.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
-@Setter
-@Getter
+@Table(name = "students")
+@Setter @Getter
 public class Student {
 
-    @Id @GeneratedValue
+    @EmbeddedId
+    StudId studId;
+
+
+    @Id
     int id;
+    @Column(table = "students")
     private String name;
+    @Column(table = "students")
     private int age;
+    @Column(table = "students")
     private Date birth;
 
     public Student(String name, int age, Date birth) {
@@ -26,4 +33,41 @@ public class Student {
     }
 
     public Student(){};
+
+}
+
+class StudId implements Serializable {
+    String name;
+    String surName;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudId studId = (StudId) o;
+        return Objects.equals(name, studId.name) &&
+                Objects.equals(surName, studId.surName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surName);
+    }
 }
